25bf371366e0cd5f07a939ac0c13ef5ee3a786db
Charles Ivia <iviacharles@gmail.com>
2024-01-15 16:41:06 +0300

    render fetched apods

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,34 @@
-import { StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, StyleSheet, FlatList } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
+import ApodListItem from "../../components/ApodListItem";
+import { Apod } from "../../types/types";
+import { getApods } from "../../api/apods";
+import FullScreenImage from "../../components/FullScreenImage";
 
-export default function TabOneScreen() {
+export default function Home() {
+  const [apods, setApods] = useState<Apod[]>([]);
+  const [activePicture, setActivePicture] = useState<string>(null);
+
+  useEffect(() => {
+    getApods().then(setApods);
+  }, []);
+
+  if (!apods.length) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <>
+      <FlatList
+        data={apods}
+        renderItem={({ item }) => <ApodListItem apod={item} onImagePress={() => setActivePicture(item.url)} />}
+      ></FlatList>
+      <FullScreenImage url={activePicture} onClose={() => setActivePicture(null)} />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
+const styles = StyleSheet.create({});
